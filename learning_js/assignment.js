5b75c5dcf940e87795642a14876288a10a1a7906
blueferanmi <blueferanmi@gmail.com>
2022-01-18 12:41:29 +0100

    completed assign

---
+++ learning_js/assignment.js
@@ -0,0 +1,11 @@
+const band ={
+    name: "BTS",
+    nationality :"korea",
+    genre: "pop",
+    members: 7,
+    formed: 2013,
+    split: false,
+    albums: [{name: "2cool4school", released: 2014} ,
+    {name:"BE",released=: 2020} ]
+
+}
---
+++ learning_js/loop.js
@@ -0,0 +1,24 @@
+// A Loop is used for performing an action that repeats itself continuosly.
+
+/*
+- for loop
+- while loop
+- do while loop
+- for in loop
+- for of loop
+*/
+
+// for loop:- this comprises of three things
+// - statement
+// - condition
+// - increment
+
+// for (let index = 0; index < 5; index++) {
+//   console.log(index);
+// }
+
+const user = {
+  name : "John",
+  surname : "Smith"
+}
+
